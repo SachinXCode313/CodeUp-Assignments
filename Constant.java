5e4fdbbdcb56cfc0f096821f831be7cbb3f0cb98
Sachin Gupta <120103804+SachinXCode313@users.noreply.github.com>
2024-09-05 15:23:43 +0530

    updated_assignment

---
+++ Constant.java
@@ -0,0 +1,32 @@
+/***
+ * This class is used for connecting the MethodsToImplement class for taking the inputs from user.
+ */
+
+package Assignment_2;
+
+public final class Constant {
+    
+    final String ENTER_INPUT = "Choose a method to execute : ";
+    final String Append = "1- Append";
+    final String CountWord = "2- CountWord";
+    final String Replace = "3- Replace";
+    final String isPalindrome = "4- isPalindome";
+    final String Splice = "5- Splice";
+    final String Split = "6- Split";
+    final String maxRepeat = "7- MaxRepeatingCharacter";
+    final String Sort = "8- Sort";
+    final String Shift = "9- Shift";
+    final String Reverse = "10- Reverse";
+    final String ENTER_SAMPLE = "Enter the number of the method which you want to perform.";
+    
+    final String INVALID_INPUT = "Invalid Input";
+    
+    final String ENTER_STRING = "Enter the string : ";
+    
+    final String OUTPUT1 = "Enter the character you what replace : ";
+    final String OUTPUT2 = "Enter the replace character : ";
+    
+    final String SLICE_INPUT1 = "Start";
+    final String SLICE_INPUT2 = "Length";
+}
+
